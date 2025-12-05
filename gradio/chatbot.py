4a9236c24a6d488649be41bde65342fb25c795c7
binblee <binblee@users.noreply.github.com>
2025-12-05 14:06:14 +0800

    update dependencies to latest

--- gradio/chatbot.py
+++ gradio/chatbot.py
@@ -1,10 +1,13 @@
 import random
 import gradio as gr
+print(f"Running Gradio version: {gr.__version__}")
 
 def random_response(message, history):
     return random.choice(["Yes", "No"])
 
-demo = gr.ChatInterface(random_response, type="messages", autofocus=False)
+demo = gr.ChatInterface(
+    random_response, 
+    autofocus=False)
 
 if __name__ == "__main__":
     demo.launch()
